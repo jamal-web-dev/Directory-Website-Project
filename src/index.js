6285d46807b6788190f3a8d8e9b162c62342c668
Jamal <jamaly381@gmail.com>
2026-02-21 10:38:51 +0100

    Completing The Home Page, With animations

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 // HEADER: SHOWING MENU ON CLICK
 const menuIcon = document.querySelector(".menu-icon");
 const nav = document.querySelector(".mobile-nav");
+const header = document.querySelector("header")
 
 menuIcon.addEventListener("click", ()=>{
     nav.classList.toggle("mobile-nav-active");
+})
+window.addEventListener("scroll", ()=>{
+    if(window.scrollY > 90){
+        header.style.backgroundColor = "#212529";
+    }else{
+        header.style.backgroundColor = "transparent";
+    }
 })
